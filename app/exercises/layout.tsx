5e0588a274dbdc389d03f7f911a79769fbd849cc
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2024-02-10 00:30:34 +0900

    Add pages (#1)

---
+++ app/exercises/layout.tsx
@@ -0,0 +1,9 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+export default function ExercisesLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <ScrollArea className="h-full py-6 pr-6">{children}</ScrollArea>
+}
