848c4e254ede18456484bcca6b36b97b044fb3e6
Kwanghyun On <kwanghyun.on@gmail.com>
2024-03-02 00:19:21 +0900

    Fix useLocalStorageState to handle server-side rendering

--- lib/useLocalStorageState.ts
+++ lib/useLocalStorageState.ts
@@ -7,11 +7,13 @@ export default function useLocalStorageState<T>(
   defaultValue: T,
 ): [T, Dispatch<SetStateAction<T>>] {
   const [state, setState] = useState<T>(() => {
+    if (typeof window === 'undefined') return defaultValue
     const storedValue = localStorage.getItem(key)
     return storedValue ? JSON.parse(storedValue) : defaultValue
   })
 
   useEffect(() => {
+    if (typeof window === 'undefined') return
     localStorage.setItem(key, JSON.stringify(state))
   }, [key, state])
 
